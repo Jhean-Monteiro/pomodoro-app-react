44b6f1849fb0b3f2dc426818616da201afd27389
Jhean-Monteiro <jheanmonteiro.2019@gmail.com>
2026-06-19 15:43:08 -0300

    first commit

--- README.md
+++ README.md
@@ -72,3 +72,4 @@ export default defineConfig([
 ])
 ```
 # pomodoro-app-react
+# teste
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-
 export default defineConfig({
   plugins: [react()],
+  base: "/pomodoro/", // Aponta para o nome do repositório no GitHub Pages
   resolve: {
     dedupe: ["react", "react-dom"], // força uma única cópia
   },
