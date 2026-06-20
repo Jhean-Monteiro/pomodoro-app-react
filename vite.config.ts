65d4cb99910f1ab4b26f465d01073f9c2240a47b
Jhean Monteiro <jheanmonteiro.2019@gmail.com>
2026-06-20 16:24:29 -0300

    Remove base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/pomodoro/", // Aponta para o nome do repositório no GitHub Pages
   resolve: {
     dedupe: ["react", "react-dom"], // força uma única cópia
   },
